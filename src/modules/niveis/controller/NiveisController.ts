c63295d450e8f4b414d8288bce1b5ea3209e2f8e
Leonardo Lima <leonardo.b.lima1@gmail.com>
2022-01-03 12:31:54 -0300

    criaçao da rota de criaçao de nivel com validaçao celebrate

--- src/modules/niveis/controller/NiveisController.ts
+++ src/modules/niveis/controller/NiveisController.ts
@@ -1,11 +1,22 @@
-import { Request, Response } from "express";
+import { Request, response, Response } from "express";
 
+import CreateNivelService from "../services/CreateNivelService";
 import ListNiveisService from "../services/ListNiveisService";
 
+interface IRequest {
+  numero: number;
+  nome: string;
+  descricao: string;
+}
 class NiveisController {
   async index(request: Request, response: Response): Promise<Response> {
     const niveis = await ListNiveisService.execute();
     return response.status(200).json(niveis);
+  }
+
+  async create({ nome, numero, descricao }: IRequest): Promise<Response> {
+    const nivel = await CreateNivelService.execute({ nome, numero, descricao });
+    return response.status(201).json(nivel);
   }
 }
 
--- src/modules/niveis/router/Niveis.routes.ts
+++ src/modules/niveis/router/Niveis.routes.ts
@@ -1,3 +1,4 @@
+import { celebrate, Joi, Segments } from "celebrate";
 import { Router } from "express";
 
 import NiveisController from "../controller/NiveisController";
@@ -5,5 +6,15 @@ import NiveisController from "../controller/NiveisController";
 const niveisRoutes = Router();
 
 niveisRoutes.get("/", NiveisController.index);
+niveisRoutes.post(
+  "/",
+  celebrate({
+    [Segments.BODY]: {
+      nome: Joi.string().required(),
+      numero: Joi.number().required(),
+    },
+  }),
+  NiveisController.create
+);
 
 export default niveisRoutes;
